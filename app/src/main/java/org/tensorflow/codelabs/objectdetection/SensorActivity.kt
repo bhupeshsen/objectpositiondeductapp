376f951766a27297f87b8089ed36043c4b98f7e6
bhupesh sen <bhupeshsen11@gmail.com>
2021-07-21 13:50:59 +0530

    angle calculate

--- app/src/main/java/org/tensorflow/codelabs/objectdetection/SensorActivity.kt
+++ app/src/main/java/org/tensorflow/codelabs/objectdetection/SensorActivity.kt
@@ -1,30 +1,35 @@
 package org.tensorflow.codelabs.objectdetection
 
+
 import android.annotation.SuppressLint
 import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class SensorActivity : AppCompatActivity() , SensorEventListener {
+class SensorActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var sensorManager: SensorManager
     private val accelerometerReading = FloatArray(3)
     private val magnetometerReading = FloatArray(3)
     private val rotationMatrix = FloatArray(9)
     private val orientationAngles = FloatArray(3)
     lateinit var tvPosition: TextView
+    lateinit var tvDegree: TextView
 
+    private val NS2S = 1.0f / 1000000000.0f
+    private val deltaRotationVector = FloatArray(4) { 0f }
+    private var timestamp: Float = 0f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sensor)
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        tvPosition =findViewById(R.id.tvPosition) as TextView
+        tvPosition = findViewById(R.id.tvPosition) as TextView
+        tvDegree = findViewById(R.id.tvDegree) as TextView
+
     }
 
     override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
@@ -44,21 +49,22 @@ class SensorActivity : AppCompatActivity() , SensorEventListener {
         // readings again.
         sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
             sensorManager.registerListener(
-                this,
-                accelerometer,
-                SensorManager.SENSOR_DELAY_NORMAL,
-                SensorManager.SENSOR_DELAY_UI
+                    this,
+                    accelerometer,
+                    SensorManager.SENSOR_DELAY_NORMAL,
+                    SensorManager.SENSOR_DELAY_UI
             )
         }
         sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)?.also { magneticField ->
             sensorManager.registerListener(
-                this,
-                magneticField,
-                SensorManager.SENSOR_DELAY_NORMAL,
-                SensorManager.SENSOR_DELAY_UI
+                    this,
+                    magneticField,
+                    SensorManager.SENSOR_DELAY_NORMAL,
+                    SensorManager.SENSOR_DELAY_UI
             )
         }
     }
+
     override fun onPause() {
         super.onPause()
 
@@ -75,8 +81,19 @@ class SensorActivity : AppCompatActivity() , SensorEventListener {
         } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
             System.arraycopy(event.values, 0, magnetometerReading, 0, magnetometerReading.size)
         }
-        tvPosition.text="X value : "+event.values[0].toString()+" \nY value : "+event.values[1].toString()+"\nZ value : "+event.values[2].toString()
+        tvPosition.text = "X value : " + event.values[0].toString() + " \nY value : " + event.values[1].toString() + "\nZ value : " + event.values[2].toString()
+        val x = event.values[0]
+        val y = event.values[1]
+        val z = event.values[2]
+        val angle = Math.atan2(y.toDouble(), Math.sqrt(x * x + z * z.toDouble())) / (Math.PI / 180)
+        tvDegree.text = "Degree : "+angle
 
+        // mam also check this link i hope it will be helpful
+        //https://stackoverflow.com/questions/14477133/how-to-use-android-sensor-event-to-determine-if-device-is-facing-up-or-down
+        //https://developer.android.com/guide/topics/sensors/sensors_motion#sensors-motion-rotate
+        //https://stackoverflow.com/questions/31361328/how-do-i-get-the-angle-degree-of-the-phone-with-sensor-s
+        //https://stackoverflow.com/questions/11175599/how-to-measure-the-tilt-of-the-phone-in-xy-plane-using-accelerometer-in-android
+        //https://stackoverflow.com/questions/13350622/getting-rotation-degree-from-gyroscope-android-sensor
     }
 
     // Compute the three orientation angles based on the most recent readings from
@@ -84,10 +101,10 @@ class SensorActivity : AppCompatActivity() , SensorEventListener {
     fun updateOrientationAngles() {
         // Update rotation matrix, which is needed to update orientation angles.
         SensorManager.getRotationMatrix(
-            rotationMatrix,
-            null,
-            accelerometerReading,
-            magnetometerReading
+                rotationMatrix,
+                null,
+                accelerometerReading,
+                magnetometerReading
         )
 
         // "rotationMatrix" now has up-to-date information.
